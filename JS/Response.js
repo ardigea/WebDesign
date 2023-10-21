e1ce6088ccc561ce871469697f3f67f394a58872
Hadirman Gea <hadirmangea25@gmail.com>
2023-10-22 01:53:04 +0700

    Bot

---
+++ JS/Response.js
@@ -0,0 +1,8 @@
+const responseobject=
+{
+    hai:"hai juga, saya diva, ada yang bisa saya bantu?",
+    ada:"saya akan mengirimkan wa admin saya.",
+    baiklah:"ok, ketik ya jika mau.",
+    ya:"Silahkan hubungi WhatsApp 082114594005",
+    terimakasih:"sama-sama"
+};
